f3a9f1f961c23530145cfcfa63f44ae4e11346fe
PereAlbertiAguilo <p.albertiaguilo@gmail.com>
2024-03-14 19:20:00 +0100

    navbar fade about dropdown animations

--- app.js
+++ app.js
@@ -1,20 +1,63 @@
 const dropdownMenu = document.querySelector('.dropdown__menu');
 const dropdownBtn = document.querySelector('.dropdown_btn');
+const dropDownIcon = document.querySelector('.dropdown_icon');
 const dropdownIcon = document.querySelectorAll('#navbar__show__icon');
 
 const serviciosDropdownMenu = document.querySelector('.servicios__dropdown__menu');
 const serviciosDropdownBtn = document.querySelector('#servicios__dropdown__btn');
 
+const hideMenu = document.querySelector('.hide');
+const hideServiciosMenu = document.querySelector('.serviciosHide');
+
 const carouselBtns = document.querySelectorAll('[data-carousel-btn]')
+
+const navbar = document.querySelector('#home');
+
+var lastScrollTop = 0;
+
+// Navabar hide on scroll
+
+window.addEventListener("scroll", () =>{
+    var scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if(scrollTop > lastScrollTop){
+        navbar.style.opacity = '0';
+    }else{
+        navbar.style.opacity = '1';
+    }
+    navbar.style.transition = "0.3s"
+    lastScrollTop = scrollTop;
+})
 
 //Navbar dropdown
 
 dropdownBtn.addEventListener('click', () =>{
-    dropdownMenu.classList.toggle('hide')
+
+    dropDownIcon.classList.toggle('fadeIn');
+    dropDownIcon.classList.toggle('fadeIn2');
+    
+    hideMenu.classList.toggle('hide');
+    hideMenu.classList.toggle('show');
+
+    hideMenu.classList.remove('hidden');
+    
+    if(hideServiciosMenu.classList.contains('serviciosHide')){
+        hideServiciosMenu.classList.add('hidden');
+    }
+    
+    dropdownMenu.classList.toggle('slideFromRight');
+    dropdownMenu.classList.toggle('slideToRight');
 })
 
+//Navbar dropdown dropdown
+
 serviciosDropdownBtn.addEventListener('click', () =>{
-    serviciosDropdownMenu.classList.toggle('hide')
+    hideServiciosMenu.classList.toggle('serviciosHide');
+    hideServiciosMenu.classList.toggle('serviciosShow');
+    hideServiciosMenu.classList.remove('hidden');
+
+    serviciosDropdownMenu.classList.toggle('slideFromRightOffset');
+    serviciosDropdownMenu.classList.toggle('slideToRightOffset');
 })
 
 
